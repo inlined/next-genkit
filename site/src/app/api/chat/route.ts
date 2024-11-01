8b496870455c67d3528c6900328250b27233220d
Thomas Bouldin <inlined@google.com>
2024-11-01 14:03:37 -0700

    production builds build locally

--- site/src/app/api/chat/route.ts
+++ site/src/app/api/chat/route.ts
@@ -1,14 +1,10 @@
-import type { NextApiRequest, NextApiResponse} from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { text } from "stream/consumers";
 import { chat } from "@/flows/chat";
 
 // TODO: make util function
-export async function POST(req: NextApiRequest) {
-  const t = await text(req.body);
-  console.log(`Text is ${t}`);
-  const json = JSON.parse(t);
-  const result = await chat(json);
+export async function POST(req: NextRequest) {
+  const result = await chat(await req.json());
   console.log(`Result is ${result}`);
   return NextResponse.json({result});
 }
